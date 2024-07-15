e8f84a91f532bbed8ef8d91bdaa76010a4f6adf1
Leonardo Flórez-Valencia <florez-l@javeriana.edu.co>
2024-07-15 16:16:23 -0500

    ...

--- prototypes/python3/scripts/01_random_frames.py
+++ prototypes/python3/scripts/01_random_frames.py
@@ -8,24 +8,26 @@ import PUJ.CGraf
 
 size = ( 100, 100 )
 color_depth = 'rgb'
-total_time = 5
+total_time = 15
 fps = 24
 
 frame = PUJ.CGraf.FrameBuffer( size, color_depth )
-# diff  = PUJ.CGraf.FrameBuffer( size, color_depth )
-# diff -= frame
+diff  = PUJ.CGraf.FrameBuffer( size, color_depth )
+diff -= frame
 
 total_frames = fps * total_time
 pad = int( math.ceil( math.log10( total_frames ) ) )
 t_off = 1.0 / ( total_frames - 1 )
 t = 0
 for frame_id in range( total_frames ):
+  frame += diff * t
+  frame.cast( )
+
+  fname = 'frame_' + str( frame_id ).rjust( pad, '0' )
+  print( fname, 'total = ' + str( total_frames ) )
+  frame.save_as_netpbm( fname )
+
   t += t_off
-  #   fname = 'frame_' + str( frame_id ).rjust( pad, '0' )
-  #   frame = ( diff * t ) + end
-  #   frame.cast( )
-  #   print( fname, 'total = ' + str( total_frames ) )
-  #   frame.save_as_netpbm( fname )
 # end for
 
 ## eof - $RCSfile$
