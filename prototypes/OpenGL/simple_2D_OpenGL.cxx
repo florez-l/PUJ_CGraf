0572c5dd8419e74acbad2ab0bdbaca79ae8caab1
Leonardo Florez-Valencia <florez-l@javeriana.edu.co>
2024-07-30 21:16:58 -0500

    ...

---
+++ prototypes/OpenGL/simple_2D_OpenGL.cxx
@@ -0,0 +1,78 @@
+// =========================================================================
+// @author Leonardo Florez-Valencia (florez-l@javeriana.edu.co)
+// =========================================================================
+
+#include <array>
+#include <iostream>
+#include <GL/glut.h>
+#include <GL/glu.h>
+#include <GL/gl.h>
+
+/**
+ */
+int main_window_id = -1;
+std::array< float, 4 > bg_color( { 1, 1, 1, 0 } );
+
+/**
+ */
+void draw_triangle( const std::array< float, 3 >& color = { 0, 0, 0 } )
+{
+  glColor3fv( color.data( ) );
+
+  glBegin( GL_LINES );
+  {
+    glVertex2f( 0, 0 );
+    glVertex2f( 1, 0 );
+
+    glVertex2f( 1, 0 );
+    glVertex2f( 0, 1 );
+
+    glVertex2f( 0, 1 );
+    glVertex2f( 0, 0 );
+  }
+  glEnd( );
+}
+
+/**
+ */
+void display( )
+{
+  glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+  draw_triangle( );
+
+  glutSwapBuffers( );
+  glutPostRedisplay( );
+}
+
+/**
+ */
+void keyboard( unsigned char key, int x, int y )
+{
+  if( key == 27 ) // ESC
+  {
+    if( main_window_id != -1 )
+      glutDestroyWindow( main_window_id );
+  } // end if
+}
+
+/**
+ */
+int main( int argc, char** argv )
+{
+  glutInitWindowSize( 700, 700 );
+  glutInitWindowPosition( 0, 0 );
+  glutInit( &argc, argv );
+  glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA );
+  main_window_id = glutCreateWindow( "window" );
+
+  glClearColor( bg_color[ 0 ], bg_color[ 1 ], bg_color[ 2 ], bg_color[ 3 ] );
+  glutDisplayFunc( display );
+  glutKeyboardFunc( keyboard );
+
+  glutMainLoop( );
+
+  return( EXIT_SUCCESS );
+}
+
+// eof - $RCSfile$
---
+++ prototypes/OpenGL/simple_2D_OpenGL.py
@@ -0,0 +1,79 @@
+## =========================================================================
+## @author Leonardo Florez-Valencia (florez-l@javeriana.edu.co)
+## =========================================================================
+
+import sys
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL import *
+
+'''
+'''
+bg_color = ( 1, 1, 1, 0 )
+main_window_id = None
+
+'''
+'''
+def draw_triangle( color = ( 0, 0, 0 ) ):
+  glColor3fv( color )
+
+  glBegin( GL_LINES )
+
+  glVertex2f( 0, 0 )
+  glVertex2f( 1, 0 )
+
+  glVertex2f( 1, 0 )
+  glVertex2f( 0, 1 )
+
+  glVertex2f( 0, 1 )
+  glVertex2f( 0, 0 )
+
+  glEnd( )
+# end def
+
+'''
+'''
+def display( ):
+  glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT )
+
+  draw_triangle( )
+
+  glutSwapBuffers( )
+  glutPostRedisplay( )
+# end def
+
+'''
+'''
+def keyboard( key, x, y ):
+  global main_window_id
+
+  if key == b'\x1b': # ESC
+    if not main_window_id is None:
+      glutDestroyWindow( main_window_id )
+    # end if
+  # end if
+# end def
+
+'''
+'''
+def main( argv ):
+  global main_window_id
+
+  glutInitWindowSize( 700, 700 )
+  glutInitWindowPosition( 0, 0 )
+  glutInit( argv )
+  glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA )
+  main_window_id = glutCreateWindow( 'window' )
+
+  glClearColor( bg_color[ 0 ], bg_color[ 1 ], bg_color[ 2 ], bg_color[ 3 ] )
+  glutDisplayFunc( display )
+  glutKeyboardFunc( keyboard )
+
+  glutMainLoop( )
+# end def
+
+if __name__ == '__main__':
+  main( sys.argv )
+# end if
+
+## eof - $RCSfile$
