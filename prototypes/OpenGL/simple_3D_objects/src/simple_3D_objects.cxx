34aff51896320835f35955e11deaf55e9aaf6fd1
Leonardo Florez Valencia <leonardo@camelot.camelot>
2024-08-27 21:31:51 -0500

    ...

--- prototypes/OpenGL/simple_3D_objects/src/simple_3D_objects.cxx
+++ prototypes/OpenGL/simple_3D_objects/src/simple_3D_objects.cxx
@@ -2,72 +2,121 @@
 // @author Leonardo Florez-Valencia (florez-l@javeriana.edu.co)
 // =========================================================================
 
-#include <fstream>
 #include <iostream>
-#include <sstream>
+#include <PUJ/CGraf/Scene.h>
+#include <GL/glut.h>
 
-#include <string>
+/**
+ */
+class Application
+{
+public:
+  using Self = Application;
 
-#include <vector>
+public:
+  static void init( int argc, char** argv );
+  static void go( );
 
+private:
+  static void cbk_idle( );
+  static void cbk_display( );
+  static void cbk_keyboard( unsigned char key, int x, int y );
+  static void cbk_reshape( int width, int height );
+
+private:
+  inline static int s_WindowSize[ 2 ] = { 700, 700 };
+  inline static int s_MainWindowId { -1 };
+
+  inline static PUJ::CGraf::Scene* s_Scene { nullptr };
+};
+
+// -------------------------------------------------------------------------
 int main( int argc, char** argv )
 {
-
-  using TReal = float;
-  using TNat = unsigned short;
-  
-  std::vector< TReal > points;
-  std::vector< TNat > indices;
-  std::vector< TNat > sizes;
-
-  std::ifstream fs( argv[ 1 ], std::ios::in | std::ios::binary | std::ios::ate );
-  std::size_t fl = fs.tellg( );
-  if( fl == 0 )
-  {
-    // error handling here
-  }
-  fs.seekg( 0, std::ios::beg );
-  std::string str( fl, '\0' );
-  if( fs.is_open( ) )
-    fs.read( &str[ 0 ], fl );
-  fs.close( );
-  std::istringstream ds( str );
-
-  std::string line;
-  while( std::getline( ds, line ) )
-  {
-    std::size_t b = str.find_first_not_of( " \t" );
-
-    if( line.data( )[ b ] != '#' && line.data( )[ b ] != '\0' )
-    {
-      std::istringstream ls( line.data( ) + b );
-      std::string cmd;
-      ls >> cmd;
-
-      if( cmd == "v" )
-      {
-        TReal x, y, z;
-        ls >> x >> y >> z;
-        points.push_back( x );
-        points.push_back( y );
-        points.push_back( z );
-      }
-      else if( cmd == "f" )
-      {
-        TNat i;
-        unsigned long long s = indices.size( );
-        while( ls >> i )
-          indices.push_back( i );
-        sizes.push_back( indices.size( ) - s );
-      } // end if
-    } // end if
-  } // end while
-
-  points.shrink_to_fit( );
-  indices.shrink_to_fit( );
-  sizes.shrink_to_fit( );
-
+  Application::init( argc, argv );
+  Application::go( );
   return( EXIT_SUCCESS );
+}
+
+// -------------------------------------------------------------------------
+void Application::
+init( int argc, char** argv )
+{
+  glutInitWindowSize( Self::s_WindowSize[ 0 ], Self::s_WindowSize[ 1 ] );
+  glutInitWindowPosition( 0, 0 );
+  glutInit( &argc, argv );
+  glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA );
+  Self::s_MainWindowId = glutCreateWindow( "window" );
+
+  if( Self::s_Scene != nullptr )
+    delete Self::s_Scene;
+  Self::s_Scene = new PUJ::CGraf::Scene( argc, argv );
+  Self::s_Scene->init( );
+
+  glutIdleFunc( Self::cbk_idle );
+  glutDisplayFunc( Self::cbk_display );
+  glutKeyboardFunc( Self::cbk_keyboard );
+  glutReshapeFunc( Self::cbk_reshape );
+}
+
+// -------------------------------------------------------------------------
+void Application::
+go( )
+{
+  glutMainLoop( );
+}
+
+// -------------------------------------------------------------------------
+void Application::
+cbk_idle( )
+{
+  glutPostRedisplay( );
+}
+
+// -------------------------------------------------------------------------
+void Application::
+cbk_display( )
+{
+  glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+  glMatrixMode( GL_MODELVIEW );
+  glLoadIdentity( );
+  Self::s_Scene->draw( );
+
+  glutSwapBuffers( );
+}
+
+// -------------------------------------------------------------------------
+void Application::
+cbk_keyboard( unsigned char key, int x, int y )
+{
+  if( key == 27 ) // ESC
+  {
+    if( Self::s_MainWindowId != -1 )
+    {
+      glutDestroyWindow( Self::s_MainWindowId );
+      Self::s_MainWindowId = -1;
+      if( Self::s_Scene != nullptr )
+        delete Self::s_Scene;
+      Self::s_Scene = nullptr;
+    } // end if
+  } // end if
+}
+
+// -------------------------------------------------------------------------
+void Application::
+cbk_reshape( int width, int height )
+{
+  Self::s_WindowSize[ 0 ] = width;
+  Self::s_WindowSize[ 1 ] = height;
+
+  glViewport( 0, 0, Self::s_WindowSize[ 0 ], Self::s_WindowSize[ 1 ] );
+
+  glMatrixMode( GL_PROJECTION );
+  glLoadIdentity( );
+  Self::s_Scene->project( Self::s_WindowSize[ 0 ], Self::s_WindowSize[ 1 ] );
+
+  glutPostRedisplay( );
 }
 
 // eof - $RCSfile$
